096f49a79fac0e1aed1fd363994d83b4e265cdf4
Michael Leli <lelimike@gmail.com>
2022-10-09 19:04:25 +0300

    add order views

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import OrderItem
+from .forms import OrderCreateForm
+from .tasks import order_created
+from cart.cart import Cart
 
-# Create your views here.
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                        product=item['product'],
+                                        price=item['price'],
+                                        quantity=item['quantity'])
+            # clear the cart
+            cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect for payment
+            return redirect(reverse('payment:get_mpesa_number'))
+    else:
+        form = OrderCreateForm()
+    return render(request,
+                  'orders/create.html',
+                  {'cart': cart, 'form': form})
+    
