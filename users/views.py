621951c4bb8997a2a7a1c444f963fd650170567c
Michael Leli <lelimike@gmail.com>
2022-10-09 13:27:52 +0300

    views /urls for address

--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView,DetailView,TemplateView,UpdateView
+#local imports
+from .forms import  AddressForm,UpdateUserForm
+from .models import Address,User
 
-# Create your views here.
+
+
+
+class AddressCreateView(LoginRequiredMixin,CreateView):
+    model = Address
+    form_class = AddressForm
+    template_name = 'users/address_form.html'
+    success_url = reverse_lazy('users:address_list')
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['addresses'] = Address.objects.filter(user=self.request.user)
+        return context
