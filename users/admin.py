a09725a27c93b6eb6094b7066213cec6c09e29c7
Michael Leli <lelimike@gmail.com>
2022-10-09 12:41:35 +0300

    edited user form

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,11 @@
 from allauth.account.forms import SignupForm 
 from django import forms 
-from .models import User,Profile,MedicProfile
+from .models import User,Address
 
 class CustomSignupForm(SignupForm):
     first_name = forms.CharField(max_length=30, label='First Name')
     last_name = forms.CharField(max_length=30, label='Last Name')
-    is_medic = forms.BooleanField(required=False,label='Are you a medic? Tick if yes')
+    phone_number = forms.CharField(max_length=30, label='Phone Number')
     
     
     def save(self,request):
@@ -13,50 +13,34 @@ class CustomSignupForm(SignupForm):
         
         first_name = self.cleaned_data['first_name']
         last_name = self.cleaned_data['last_name']
-        is_medic=self.cleaned_data['is_medic']
+        phone_number = self.cleaned_data['phone_number']
+       
         
         
         user.first_name = first_name
         user.last_name = last_name
-        user.is_medic = is_medic
+        user.phone_number = phone_number
+        
         
         user.save()
         return user
     
 
     
-class ProfileForm(forms.ModelForm):
-    phone_number = forms.CharField(max_length=20, label='Phone Number')
-    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
-
+class AddressForm(forms.ModelForm):
+   
     class Meta:
-        model = Profile
-        fields = ['phone_number','gender','avatar', 'bio','birthdate',]
+        model = Address
+        fields = ['full_name','post_code','address_line','address_line2','town_city','delivery_instructions']
 
 
-class MedicProfileForm(forms.ModelForm):
-    phone_number = forms.CharField(max_length=20, label='Phone Number')
-    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
-    registration_number = forms.CharField(max_length=20, label='Registration Number')
-    cadre = forms.CharField(max_length=20, label='Cadre')
-    
 
-    class Meta:
-        model = MedicProfile
-        fields = ['phone_number','gender','avatar', 'bio','birthdate','registration_number', 'cadre']
-        
         
     
 class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
         # Add here the fields you want to be present in the update form
-        fields = ('first_name', 'last_name','is_medic', 'email')
+        fields = ('first_name', 'last_name', 'email', 'phone_number')
         
         
-class ProfileUpdateForm(forms.ModelForm):
-    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
-    
-    class Meta:
-        model = Profile
-        fields=('avatar','bio','birthdate')
