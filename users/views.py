df3ab749b07028c4e2bfe65e2486f8c5c31214ca
Michael Leli <lelimike@gmail.com>
2022-10-10 06:17:41 +0300

    updated address views

--- users/views.py
+++ users/views.py
@@ -5,50 +5,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView,DetailView,TemplateView,UpdateView
 
-from cart.forms import CartAddProductForm
-
-from .forms import  AddressForm,UpdateUserForm
-from .models import Address,User
-from .models import Category, Product
-
-
-def product_list(request, category_slug=None):
-    category = None
-    categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
-    if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
-    return render(request,
-                  'shop/product/list.html',
-                  {'category': category,
-                   'categories': categories,
-                   'products': products})
-
-
-def product_detail(request, id, slug):
-    product = get_object_or_404(Product,
-                                id=id,
-                                slug=slug,
-                                available=True)
-    cart_product_form = CartAddProductForm()
-    return render(request,
-                  'shop/product/detail.html',
-                  {'product': product,
-                   'cart_product_form': cart_product_form})
-    
-    
-
-class ProductCreateView(LoginRequiredMixin, CreateView):
-    model = Product
-    fields = ['name', 'category', 'description', 'price', 'available', 'image']
-    template_name = 'shop/product/create.html'
-    success_url = reverse_lazy('shop:product_list')
-    
-    def form_valid(self, form):
-        form.instance.owner = self.request.user
-        return super().form_valid(form)
-
+from.models import Address
 
 
 class AddressCreateView(LoginRequiredMixin,CreateView):
