242b8cd08ca7f8fe7bec5c38d5923915c9bc0e21
Michael Leli <lelimike@gmail.com>
2022-10-10 06:18:14 +0300

    user views

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView,DetailView,TemplateView,UpdateView
 
-from.models import Address
+from.models import Address,User
 
 
 class AddressCreateView(LoginRequiredMixin,CreateView):
