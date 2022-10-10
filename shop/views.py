b66cfc2e051a764242abb5611931f5f1df8a6c62
Michael Leli <lelimike@gmail.com>
2022-10-10 06:21:48 +0300

    products/ category views

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    DetailView,ListView,TemplateView,
+    CreateView,UpdateView,DeleteView
+)
 
-# Create your views here.
+
+from cart.forms import CartAddProductForm
+from .models import Category, Product
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product,
+                   'cart_product_form': cart_product_form})
+    
+    
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    fields = ['name', 'category', 'description', 'price', 'available', 'image']
+    template_name = 'shop/product/create.html'
+    success_url = reverse_lazy('shop:product_list')
+    
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
