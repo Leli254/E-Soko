d7e87f3762d57eb2b7ab1645b3679a13016c5752
Michael Leli <lelimike@gmail.com>
2022-10-11 15:13:20 +0300

    celery

--- orders/models.py
+++ orders/models.py
@@ -8,7 +8,6 @@ class Order(models.Model):
     address = models.CharField(max_length=250)
     postal_code = models.CharField(max_length=20)
     city = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=12, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
