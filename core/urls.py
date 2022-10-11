134d4c736b73de388216a0e3cbf74cd42238740e
Michael Leli <lelimike@gmail.com>
2022-10-11 14:18:50 +0300

    add shop static files

--- core/urls.py
+++ core/urls.py
@@ -22,3 +22,8 @@ urlpatterns = [
     path('payment/', include('payments.urls', namespace='payment')),
     path('shop/', include('shop.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
