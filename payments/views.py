cedd20047a6b613046ae164db094f4513d8ea9bd
Michael Leli <lelimike@gmail.com>
2022-10-24 07:13:21 +0300

    rename stripe payment view

--- payments/views.py
+++ payments/views.py
@@ -226,7 +226,7 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 stripe.api_version = settings.STRIPE_API_VERSION
 
 
-def payment_process(request):
+def stripe_payment_process(request):
     order_id = request.session.get('order_id', None)
     order = get_object_or_404(Order, id=order_id)
 
