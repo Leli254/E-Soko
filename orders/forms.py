8e2ab11adfa7309d455349a42f8b7ad315973cdd
Michael Leli <lelimike@gmail.com>
2022-10-29 12:19:52 +0300

    added model form labels /widgets

--- orders/forms.py
+++ orders/forms.py
@@ -1,8 +1,29 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 from .models import Order
+from users.models import Address
+
 
 
 class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['address','pickup_station']
+
+
+
+
+    address = forms.ModelChoiceField(
+        
+        widget=forms.RadioSelect,
+        label='Select your shipping Address',
+        queryset=Address.objects.all(),
+        required=False
+        )
+    
+    pickup_station = forms.ModelChoiceField(
+        widget=forms.RadioSelect,
+        label='Select your pickup station',
+        queryset=Address.objects.all(),
+        required=False
+        )
