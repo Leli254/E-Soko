25b9e376e531fe18340663c77e9d1869ac0c6634
Michael Leli <lelimike@gmail.com>
2022-10-31 07:37:46 +0300

    refactored paths

--- shop/urls.py
+++ shop/urls.py
@@ -4,9 +4,8 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('<slug:category_slug>/', views.product_list,
-         name='product_list_by_category'),
-    path('<int:id>/<slug:slug>/', views.product_detail,
-         name='product_detail'),
+    path('', views.ProductListView.as_view(), name='product_list'),
+    path('<slug:category_slug>/', views.ProductListView.as_view(),name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.ProductDetailView.as_view(),    name='product_detail'),
+    path('create/', views.ProductCreateView.as_view(), name='product_create'),
 ]
