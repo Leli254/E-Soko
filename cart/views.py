b5d3185cfcafe571ee43ebf92ec89bf05d380e95
leli <lelimike@gmail.com>
2022-11-16 11:04:15 +0300

    include coupon

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
-from django.views.generic import View
+from django.views.generic import View,DetailView
 
 from shop.models import Product
+from coupons.forms import CouponApplyForm
+
 from .cart import Cart
 from .forms import CartAddProductForm
 
@@ -34,10 +36,29 @@ def cart_detail(request):
         item['update_quantity_form'] = CartAddProductForm(initial={
                             'quantity': item['quantity'],
                             'override': True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+    coupon_apply_form = CouponApplyForm()
+    return render(
+        request, 
+        'cart/detail.html', 
+        {'cart': cart,
+         'coupon_apply_form': coupon_apply_form})
 
+#above view as class based view
+class CartDetail(View):
+    def get(self, request):
+        cart = Cart(request)
+        for item in cart:
+            item['update_quantity_form'] = CartAddProductForm(initial={
+                                'quantity': item['quantity'],
+                                'override': True})
+        coupon_apply_form = CouponApplyForm()
+        return render(
+            request, 
+            'cart/detail.html', 
+            {'cart': cart,
+             'coupon_apply_form': coupon_apply_form})
 
-#a class based view to add items to the cart withou using a form
+#a class based view to add items to the cart without using a form
 class CartAdd(View):
     def get(self, request, *args, **kwargs):
         product_id = self.kwargs['product_id']
