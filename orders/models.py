efd2369fe5c60d93e90d789ccdcb3977137629be
leli <lelimike@gmail.com>
2022-11-16 12:10:57 +0300

    add methods to calculate discount

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,12 @@
+from decimal import Decimal
+
 from django.db import models
 from django.utils.crypto import get_random_string
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 
 from shop.models import Product
+from coupons.models import Coupon
 from users.models import Address,PickupStation
 
 class Order(models.Model):
@@ -11,6 +15,10 @@ class Order(models.Model):
     pickup_station = models.ForeignKey(
         PickupStation, on_delete=models.CASCADE, null=True, blank=True)
     order_number = models.CharField(max_length=9, blank=True)
+    coupon=models.ForeignKey(
+        Coupon,related_name='orders',null=True,blank=True,on_delete=models.SET_NULL)
+    discount= models.IntegerField(
+        default=0,validators=[MinValueValidator(0),MaxValueValidator(100)])
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
@@ -24,15 +32,27 @@ class Order(models.Model):
     def __str__(self):
         return f'Order {self.id}'
     
-    
 
     def save(self, *args, **kwargs):
         if not self.order_number:
             self.order_number = get_random_string(length=9, allowed_chars='1234567890')
         super().save(*args, **kwargs)
 
-    def get_total_cost(self):
+
+    def get_total_cost_before_discount(self):
         return sum(item.get_cost() for item in self.items.all())
+
+    def get_discount(self):
+        total_cost=self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost*(self.discount / Decimal(100))
+        return Decimal(0)
+
+
+    def get_total_cost(self):
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,
