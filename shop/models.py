79ede25b08df54210763a90dfee84e00637cabc0
leli <lelimike@gmail.com>
2022-11-17 14:57:33 +0300

    review comment to allow blank and null

--- shop/models.py
+++ shop/models.py
@@ -104,7 +104,7 @@ class Review(models.Model):
     user=models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviews')
     rating=models.IntegerField()
-    comment=models.TextField()
+    comment=models.TextField(null=True, blank=True)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
 
